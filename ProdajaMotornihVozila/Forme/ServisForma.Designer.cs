29624edb41db112fae7cefd98e6e7ce1d700db48
joncic02 <sjoncic02@gmail.com>
2024-05-25 17:42:19 +0200

    Servis forme.

--- ProdajaMotornihVozila/Forme/ServisForma.Designer.cs
+++ ProdajaMotornihVozila/Forme/ServisForma.Designer.cs
@@ -28,12 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ServisForma";
+            listView1 = new ListView();
+            columnHeader1 = new ColumnHeader();
+            columnHeader2 = new ColumnHeader();
+            columnHeader3 = new ColumnHeader();
+            columnHeader4 = new ColumnHeader();
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            btnDodaj = new Button();
+            btnInfo = new Button();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // listView1
+            // 
+            listView1.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader3, columnHeader4 });
+            listView1.GridLines = true;
+            listView1.Location = new Point(6, 26);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(560, 447);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "Model";
+            columnHeader1.Width = 120;
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "Registarski broj";
+            columnHeader2.Width = 120;
+            // 
+            // columnHeader3
+            // 
+            columnHeader3.Text = "MBR izvrsioca prijema";
+            columnHeader3.Width = 120;
+            // 
+            // columnHeader4
+            // 
+            columnHeader4.Text = "Opis";
+            columnHeader4.Width = 200;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(listView1);
+            groupBox1.Location = new Point(31, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(572, 479);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Lista obavljenih servisa";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(btnDodaj);
+            groupBox2.Controls.Add(btnInfo);
+            groupBox2.Location = new Point(638, 12);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(281, 163);
+            groupBox2.TabIndex = 2;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Informacije o obavljenom servisu";
+            // 
+            // btnDodaj
+            // 
+            btnDodaj.Location = new Point(49, 99);
+            btnDodaj.Name = "btnDodaj";
+            btnDodaj.Size = new Size(182, 44);
+            btnDodaj.TabIndex = 1;
+            btnDodaj.Text = "Obavi novi servis";
+            btnDodaj.UseVisualStyleBackColor = true;
+            btnDodaj.Click += btnDodaj_Click;
+            // 
+            // btnInfo
+            // 
+            btnInfo.Location = new Point(49, 35);
+            btnInfo.Name = "btnInfo";
+            btnInfo.Size = new Size(182, 44);
+            btnInfo.TabIndex = 0;
+            btnInfo.Text = "Informacije";
+            btnInfo.UseVisualStyleBackColor = true;
+            // 
+            // ServisForma
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(953, 519);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "ServisForma";
+            Text = "ServisForma";
+            groupBox1.ResumeLayout(false);
+            groupBox2.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private ListView listView1;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+        private ColumnHeader columnHeader4;
+        private Button btnDodaj;
+        private Button btnInfo;
     }
 }
--- ProdajaMotornihVozila/Forme/VozilaForma.Designer.cs
+++ ProdajaMotornihVozila/Forme/VozilaForma.Designer.cs
@@ -35,24 +35,33 @@
             btnInfo = new Button();
             btnIzmeni = new Button();
             btnDodaj = new Button();
+            columnHeader1 = new ColumnHeader();
+            columnHeader2 = new ColumnHeader();
+            columnHeader3 = new ColumnHeader();
+            columnHeader4 = new ColumnHeader();
+            columnHeader5 = new ColumnHeader();
+            columnHeader6 = new ColumnHeader();
             groupBox1.SuspendLayout();
             groupBox2.SuspendLayout();
             SuspendLayout();
             // 
             // listaVozila
             // 
+            listaVozila.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader3, columnHeader4, columnHeader5, columnHeader6 });
+            listaVozila.GridLines = true;
             listaVozila.Location = new Point(5, 27);
             listaVozila.Name = "listaVozila";
-            listaVozila.Size = new Size(606, 195);
+            listaVozila.Size = new Size(654, 195);
             listaVozila.TabIndex = 0;
             listaVozila.UseCompatibleStateImageBehavior = false;
+            listaVozila.View = View.Details;
             // 
             // groupBox1
             // 
             groupBox1.Controls.Add(listaVozila);
             groupBox1.Location = new Point(63, 34);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(617, 228);
+            groupBox1.Size = new Size(666, 228);
             groupBox1.TabIndex = 1;
             groupBox1.TabStop = false;
             groupBox1.Text = "Lista vozila";
@@ -65,14 +74,14 @@
             groupBox2.Controls.Add(btnDodaj);
             groupBox2.Location = new Point(68, 284);
             groupBox2.Name = "groupBox2";
-            groupBox2.Size = new Size(612, 276);
+            groupBox2.Size = new Size(661, 276);
             groupBox2.TabIndex = 2;
             groupBox2.TabStop = false;
             groupBox2.Text = "Upravljanje vozilima";
             // 
             // btnObrisi
             // 
-            btnObrisi.Location = new Point(51, 221);
+            btnObrisi.Location = new Point(218, 208);
             btnObrisi.Name = "btnObrisi";
             btnObrisi.Size = new Size(227, 38);
             btnObrisi.TabIndex = 6;
@@ -82,7 +91,7 @@
             // 
             // btnInfo
             // 
-            btnInfo.Location = new Point(51, 39);
+            btnInfo.Location = new Point(218, 26);
             btnInfo.Name = "btnInfo";
             btnInfo.Size = new Size(227, 38);
             btnInfo.TabIndex = 3;
@@ -91,7 +100,7 @@
             // 
             // btnIzmeni
             // 
-            btnIzmeni.Location = new Point(51, 160);
+            btnIzmeni.Location = new Point(218, 147);
             btnIzmeni.Name = "btnIzmeni";
             btnIzmeni.Size = new Size(227, 38);
             btnIzmeni.TabIndex = 5;
@@ -100,7 +109,7 @@
             // 
             // btnDodaj
             // 
-            btnDodaj.Location = new Point(51, 98);
+            btnDodaj.Location = new Point(218, 85);
             btnDodaj.Name = "btnDodaj";
             btnDodaj.Size = new Size(227, 38);
             btnDodaj.TabIndex = 4;
@@ -108,11 +117,41 @@
             btnDodaj.UseVisualStyleBackColor = true;
             btnDodaj.Click += btnDodaj_Click;
             // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "Broj sasije";
+            columnHeader1.Width = 130;
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "Boja";
+            columnHeader2.Width = 90;
+            // 
+            // columnHeader3
+            // 
+            columnHeader3.Text = "Model";
+            columnHeader3.Width = 120;
+            // 
+            // columnHeader4
+            // 
+            columnHeader4.Text = "Tip goriva";
+            columnHeader4.Width = 90;
+            // 
+            // columnHeader5
+            // 
+            columnHeader5.Text = "Kubikaza";
+            columnHeader5.Width = 100;
+            // 
+            // columnHeader6
+            // 
+            columnHeader6.Text = "Broj motora";
+            columnHeader6.Width = 120;
+            // 
             // VozilaForma
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(769, 623);
+            ClientSize = new Size(789, 623);
             Controls.Add(groupBox2);
             Controls.Add(groupBox1);
             Name = "VozilaForma";
@@ -132,5 +171,11 @@
         private Button btnInfo;
         private Button btnObrisi;
         private Button btnIzmeni;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+        private ColumnHeader columnHeader4;
+        private ColumnHeader columnHeader5;
+        private ColumnHeader columnHeader6;
     }
 }
